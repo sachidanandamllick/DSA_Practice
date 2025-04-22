b40d3f2dd24d9e5268c4a5a085769c86480fff29
sachidanandamllick <sachidanandamallick422@gmail.com>
2025-04-22 18:38:47 +0530

    Update merge2sortedarray.js

--- Babber_450/merge2sortedarray.js
+++ Babber_450/merge2sortedarray.js
@@ -11,3 +11,34 @@ var merge = function(nums1, m, nums2, n) {
     nums1.sort((a,b)=> a-b)
     return nums1
 };
+
+
+let a = [1, 2, 4, 10];
+let b = [2, 3];
+let m = a.length;
+let n = b.length;
+
+a.length = m + n;
+
+let p1 = m - 1;
+let p2 = n - 1;
+let p = m + n - 1;
+
+while (p1 >= 0 && p2 >= 0) {
+    if (a[p1] > b[p2]) {
+        a[p] = a[p1];
+        p1--;
+    } else {
+        a[p] = b[p2];
+        p2--;
+    }
+    p--;
+}
+
+while (p2 >= 0) {
+    a[p] = b[p2];
+    p2--;
+    p--;
+}
+
+console.log(a);
